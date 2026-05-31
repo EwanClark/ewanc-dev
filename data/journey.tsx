11d054e390ed2376e16947c7ca749eb2d736db5a
Ewan Clark <ewanclark4312@gmail.com>
2026-05-31 19:34:53 +0100

    update: journey entry dates

--- data/journey.tsx
+++ data/journey.tsx
@@ -13,17 +13,17 @@ export const journeyEntries: JourneyEntry[] = [
   {
     title: "Computer Science GCSE",
     organization: "Secondary School",
-    date: "Year 9",
-    endDate: "Now",
+    date: "Year 8",
+    endDate: "Year 9",
     alternateDate: "Dec 2024",
-    alternateEndDate: "Now",
+    alternateEndDate: "May 2026",
     description: "Taking the Computer Science GCSE 2 years early, without any formal teaching.",
   },
   {
     title: "Volunteering",
     organization: "Local Church",
-    date: "Jan 2025",
-    endDate: "Now",
+    date: "Jan 2026",
+    endDate: "Apr 2026",
     description: "Volunteering at my local church, helping with their IT systems, applying my skills to real world problems.",
   },
 ];
